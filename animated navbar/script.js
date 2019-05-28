ffba86338d943c504e08e5a7739e25331454b4d8
Ritambhra Vatsya <covertlyaphrodite@gmail.com>
2019-05-28 23:32:51 +0530

    .

--- animated navbar/script.js
+++ animated navbar/script.js
@@ -9,12 +9,35 @@ function anav(){
     section.addEventListener('mouseenter',function(){
     	const id = this.getAttribute("id");
     	const navActive = document.querySelector(`a[href="#${id}"]`);
-    	navLinks.forEach(link => link.classList.remove("active"));
+    	navLinks.forEach(link => {link.classList.remove("active");
+
+    	});	
+    
     	navActive.classList.add("active");
+
+
+
 
     } );
  } );
 
+ navLinks.forEach(link=>{
+ 	link.addEventListener('click',function(){
+ 		const navActive= this;
+ 		navLinks.forEach(link => {link.classList.remove("active");
+        
+ 	});
+   navActive.classList.add("active");
+
+ });
+
+});
 }
+
+
+
+// link.addEventListener('click', function(){
+//     		link.classList.remove("active");
+//     		navActive.classList.add("active");
 
 anav();
